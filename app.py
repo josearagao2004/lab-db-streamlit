5353dcf530013def1f4c72c267a84b40ac607d96
josearagao2004 <jose.aragao@unemat.br>
2026-09-24 01:35:08 +0000

    consultando clientes

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 import streamlit as st 
+import psycopg2
+
+conn = psycopg2.connect(
+    host=st.secrets["DB_HOST"], 
+    database=st.secrets["DB_NAME"],
+    user=st.secrets["DB_USER"],
+    password=st.secrets[DB_PASSWORD],
+    portpassword=st.secrets[DB_PORT]
+)
+
+cursor = conn.cursor()
+
 
 st.title("Sistema de Cadastro")
 st.write("Bem-vindo ao sistema!")
 
+cursor.execute("SELECT nome, email FROM cliente")
+dados = cursor.fetchall()
+
+for nome, email in dados: 
+    st.write (f"{nome} - {email}")
