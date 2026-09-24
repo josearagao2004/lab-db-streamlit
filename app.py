fbbffa36e05e3977204cbd1c9033d5d0bca2bf77
josearagao2004 <jose.aragao@unemat.br>
2026-09-24 01:57:58 +0000

    Cadastro de Clientes

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ email = st.text_input("Email")
 
 if st.button("Cadastrar"):
     cursor.execute(
-        "INSERT INTO cliente (nome, cpf, email), VALUES (%s,%s,%s)",
+        "INSERT INTO cliente (nome, cpf, email) VALUES (%s,%s,%s)",
         (nome, "000000000", email)
     )
     conn.commit()
